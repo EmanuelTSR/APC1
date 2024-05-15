9f50d63c1ae44f36a501f53356e7bac7c4f5ca99
EmanuelTSR <160737789+EmanuelTSR@users.noreply.github.com>
2024-05-15 13:54:25 +0000

    codigos aula 07 e pratica 3

---
+++ praticas/pratica03/questao4.c
@@ -0,0 +1,8 @@
+//4. Faça um programa em C que leia uma tecla pressionada e determine se ela é uma letra, um dígito ou um caractere especial.
+#include <stdio.h>
+int main(){
+  
+
+
+  return 0;
+}
